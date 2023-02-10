f4ce9cc5ef982cab055abdfa7e526db1568d7aab
Martin Svátek <svatek.ma@gmail.com>
2023-02-10 12:52:28 +0100

    [create] src folder

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components";
+import { FC, useEffect } from "react";
+import { AppErrorProps } from "./error.types";
+
+const AppError: FC<AppErrorProps> = ({ error, reset }) => {
+  useEffect(() => {
+    /**
+     * @NOTE: tady muzeme poslat error do nejakeho reportovaciho systemu
+     */
+    console.error(error);
+  }, [error]);
+
+  return (
+    <>
+      <p>Something went wrong...</p>
+      <Button onClick={(): void => reset()}>Reset</Button>
+    </>
+  );
+};
+
+export default AppError;
