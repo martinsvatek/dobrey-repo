3335fb1af41f096b5011e9bb26e8efe85f982fce
Martin Svátek <svatek.ma@gmail.com>
2023-03-26 16:45:08 +0200

    [create] show status in nav menu

--- src/components/Nav/NavMenu/NavMenu.tsx
+++ src/components/Nav/NavMenu/NavMenu.tsx
@@ -10,12 +10,16 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from 'components/Button';
 import { joinClassNames } from 'global/utils';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 import { useRef } from 'react';
 import { NavLink } from './NavLink';
 import { useNavMenu } from './NavMenu.hooks';
 import styles from './NavMenu.module.scss';
 
 export const NavMenu = (): JSX.Element => {
+	const { data, status } = useSession();
+
 	const navRef = useRef<HTMLElement>(null);
 
 	const { isButtonDisabled, isMenuOpen, isRemoved, onButtonClickHandler, onLinkClickHandler } = useNavMenu(navRef);
@@ -32,30 +36,35 @@ export const NavMenu = (): JSX.Element => {
 			</Button>
 			{isMenuOpen && (
 				<nav className={joinClassNames([styles.nav, isRemoved && styles.hideAnimation])} ref={navRef}>
-					<NavLink
-						href="/learning/self-driving-car"
-						icon={<StopCircleIcon className={styles.iconLink} />}
-						onClick={onLinkClickHandler}
-						title="Self driving car"
-					/>
-					<NavLink
-						href="/learning/shortest-path"
-						icon={<ArrowPathRoundedSquareIcon className={styles.iconLink} />}
-						onClick={onLinkClickHandler}
-						title="Shortest path"
-					/>
-					<NavLink
-						href="/learning/web-scraper"
-						icon={<ArrowDownOnSquareIcon className={styles.iconLink} />}
-						onClick={onLinkClickHandler}
-						title="Web scraper"
-					/>
-					<NavLink
-						href="/learning/czech-robot"
-						icon={<CodeBracketIcon className={styles.iconLink} />}
-						onClick={onLinkClickHandler}
-						title="Czech robot"
-					/>
+					<Link className={styles.status} href={data ? '/signout' : 'signin'}>
+						{status}
+					</Link>
+					<div className={styles.iconLinks}>
+						<NavLink
+							href="/learning/self-driving-car"
+							icon={<StopCircleIcon className={styles.iconLink} />}
+							onClick={onLinkClickHandler}
+							title="Self driving car"
+						/>
+						<NavLink
+							href="/learning/shortest-path"
+							icon={<ArrowPathRoundedSquareIcon className={styles.iconLink} />}
+							onClick={onLinkClickHandler}
+							title="Shortest path"
+						/>
+						<NavLink
+							href="/learning/web-scraper"
+							icon={<ArrowDownOnSquareIcon className={styles.iconLink} />}
+							onClick={onLinkClickHandler}
+							title="Web scraper"
+						/>
+						<NavLink
+							href="/learning/czech-robot"
+							icon={<CodeBracketIcon className={styles.iconLink} />}
+							onClick={onLinkClickHandler}
+							title="Czech robot"
+						/>
+					</div>
 				</nav>
 			)}
 		</>
